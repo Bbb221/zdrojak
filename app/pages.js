bc929f325f3157ec4c4ae34cab81175bb29dfd78
Bbb221 <patmat221@centrum.sk>
2018-11-14 01:26:01 +0100

    next commit (2nd) weee

--- app/pages.js
+++ app/pages.js
@@ -1,4 +1,6 @@
 // testovaci data
+//Nejprve jsme si vytvořili testovací data v proměnné pages (v příštím díle již budeme získávat data z databáze).
+
 var pages = {
     'obchodni-podminky': {
         'title': 'Obchodní podmínky',
@@ -16,6 +18,7 @@ var pages = {
 
 /**
  * GET /pages
+ * Metoda index() zavolá res.json(), která převede objekt do formátu JSON a odešle ho zpět prohlížeči
  */
 exports.index = function(req, res){
     res.json(pages);
@@ -23,9 +26,10 @@ exports.index = function(req, res){
 
 /**
  * GET /pages/:page
+ * Metoda show() kontroluje, zda zadanou URL zná. Pokud ano, odešle do  prohlížeče data konkrétní stránky opět ve formátu JSON. Pokud nikoliv, zavolá se metoda res.send(), která vrátí prohlížeči odpověď HTTP 404 Not Found 
  */
 exports.show = function(req, res){
-    page = pages[req.params.page];
+    page = pages[req.params.page]; // Parametr req.params.page se vytvoří automaticky (page je jednotné číslo pages, vytvoří se přes modul lingo)
     if (page) {
         res.json(page);
     } else {
@@ -53,3 +57,7 @@ exports.update = function(req, res){
 exports.destroy = function(req, res){
     //...
 };
+
+/*
+	Skript spusťte příkazem node server.js. Fungovat nyní budou dvě URL http://localhost:5000/pages vrací všechny stránky, http://localhost:5000/pages/obchodni-podminky např. vrátí stránku Obchodní podmínky ve formátu JSON. Všechny metody jsou součástí objektu exports, který specifikuje, co z daného modulu bude dostupné zvenčí.
+*/
--- server.js
+++ server.js
@@ -4,3 +4,12 @@ var express  = require('express')
 
 app.resource('pages', require('./app/pages'));
 app.listen(process.env.PORT || 5000);
+//REST architektuře, která umožňuje výrazně jednodušší testování a pozdější snadné napojení např. pro aplikace pro chytré telefony přes RESTful API
+
+/*
+Nejprve načítáme framework Express a modul Express Resource, který výrazně usnadňuje vytvoření RESTful API. Dále vytvoříme novou instanci frameworku Express.
+
+Metoda app.resource() vytváří API pro daný zdroj, v našem případě pro stránky. Druhý parametr metody načítá soubor /app/pages.js, kde budou metody pro obsluhu požadavků, jak uvidíte za okamžik. Všimněte si, že není potřeba zadávat koncovku načítaného souboru.
+
+Nakonec zavoláme metodu app.listen(). Říká, že server má poslouchat na portu 5000, pokud není nastavena proměnná prostředí PORT (ta je důležitá pro Heroku).
+*/
